695a6dcdfdcfa98b068a7c3978168c3ed6232b01
Bernardo Afonso <bernardoafonso9734@gmail.com>
2022-01-29 18:48:02 -0300

    Configs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "34f6a81667a41705"; }
+/******/ 		__webpack_require__.h = function() { return "03b2066b19c199bc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
