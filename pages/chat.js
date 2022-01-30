4e171ec5bda46a774bca8023ec6bad4f92dd0544
Bernardo Afonso <bernardoafonso9734@gmail.com>
2022-01-30 13:05:52 -0300

    Template adicionado e criando funcoes

--- pages/chat.js
+++ pages/chat.js
@@ -65,9 +65,6 @@ export default function ChatPage() {
                             setMensagem(valor);
                             console.log(valor);
                         }}
-                        onKeyPress={
-                            
-                        }
                             placeholder="Insira sua mensagem aqui..."
                             type="textarea"
                             styleSheet={{
